4a38d3c48348adf861b1870b6e594a15384c1991
Blaine Bublitz <blaine.bublitz@gmail.com>
2020-02-09 11:43:11 +0200

    Update: Reduce boilerplate around Card union

--- src/models/card.rs
+++ src/models/card.rs
@@ -14,81 +14,44 @@ impl From<BattleCard> for Card {
   }
 }
 
-impl Card {
-  pub fn id(&self) -> Uuid {
+juniper::graphql_interface!(Card: Context |&self| {
+  field id() -> Uuid {
     match *self {
       Card::BattleCard(BattleCard { id, .. }) => id,
     }
   }
 
-  pub fn tcg_id(&self) -> &str {
+  field tcg_id() -> &str {
     match *self {
       Card::BattleCard(BattleCard { ref tcg_id, .. }) => tcg_id.as_str(),
     }
   }
 
-  pub fn rarity(&self) -> &CardRarity {
+  field rarity() -> &CardRarity {
     match *self {
       Card::BattleCard(BattleCard { ref rarity, .. }) => rarity,
     }
   }
 
-  pub fn number(&self) -> &str {
+  field number() -> &str {
     match *self {
       Card::BattleCard(BattleCard { ref number, .. }) => number.as_str(),
     }
   }
 
-  pub fn category(&self) -> &CardCategory {
+  field category() -> &CardCategory {
     match *self {
       Card::BattleCard(BattleCard { ref category, .. }) => category,
     }
   }
 
-  pub fn wave(&self, context: &Context) -> Wave {
-    match &*self {
-      Card::BattleCard(card) => card.wave(context),
-    }
-  }
-}
-
-juniper::graphql_interface!(Card: Context |&self| {
-  field id() -> Uuid {
-    self.id()
-  }
-
-  field tcg_id() -> &str {
-    self.tcg_id()
-  }
-
-  field rarity() -> &CardRarity {
-    self.rarity()
-  }
-
-  field number() -> &str {
-    self.number()
-  }
-
-  field category() -> &CardCategory {
-    self.category()
-  }
-
   field wave(&executor) -> Wave {
-    self.wave(&executor.context())
+    match &*self {
+      Card::BattleCard(card) => card.wave(&executor.context()),
+    }
   }
 
   instance_resolvers: |_| {
     &BattleCard => match *self { Card::BattleCard(ref card) => Some(card), _ => None },
-      // BattleCard => {
-      //   let results = battle_cards::table
-      //   .inner_join(cards::table)
-      //   .select((battle_cards::id, battle_cards::card_id, cards::tcg_id, cards::rarity, cards::number, cards::category, cards::wave_id))
-      //   .first::<BattleCard>(&context.connection);
-      //   match results {
-      //     Ok(res) => Some(res),
-      //     Err(_) => None
-      //   }
-      // }
-      // &Droid => context.droids.get(&self.id),
   }
 });
