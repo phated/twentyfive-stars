b39716f7ef9d052373e5761dfa4c9be5555bd3b6
Blaine Bublitz <blaine.bublitz@gmail.com>
2020-05-28 13:05:32 -0700

    Cleanup migrations

--- server/migrations/2020-02-22-210844_create_stratagem_cards/down.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE stratagem_cards;
--- server/migrations/2020-03-07-222737_create_pageinfo_views/down.sql
+++
@@ -1,4 +0,0 @@
-DROP VIEW cards_with_pageinfo;
-DROP VIEW battle_cards_with_pageinfo;
-DROP VIEW character_cards_with_pageinfo;
-DROP VIEW stratagem_cards_with_pageinfo;
